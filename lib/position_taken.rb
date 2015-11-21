43dabe5cc804cc76ebe0dede4954296881acfe18
mailethiesen <mailethiesen@gmail.com>
2015-11-21 22:01:14 +0000

    nitrous-backup at Sat Nov 21 22:01:14 UTC 2015

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,7 @@
 def position_taken?(board, position)
   if board[position] == " " || board[position] == "" || board[position] == nil
     return false
-  elsif board[position] == "X" || board[position] == "O"  
+  elsif board[position] == "X" || board[position] == "O"
     return true
   end
 end
